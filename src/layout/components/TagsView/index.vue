bf66265835fc92fe499cdfc687018f051c58c00c
skytangleisky <2413081441@qq.com>
2022-05-30 11:14:42 +0800

    加入FPS监控

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -205,6 +205,7 @@ export default {
   background: #fff;
   border-bottom: 1px solid #d8dce5;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .12), 0 0 3px 0 rgba(0, 0, 0, .04);
+  box-sizing:border-box;
   .tags-view-wrapper {
     .tags-view-item {
       display: inline-block;
