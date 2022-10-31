d7bb9a05a1c82526e7220cb72517c80fab3429ed
skytangleisky <2413081441@qq.com>
2022-10-31 19:35:08 +0800

    关闭设置按钮

--- src/settings.js
+++ src/settings.js
@@ -6,7 +6,7 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether show the settings right-panel
    */
-  showSettings: true,
+  showSettings: false,
 
   /**
    * @type {boolean} true | false
